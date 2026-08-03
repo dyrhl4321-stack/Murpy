b3f71feb6312af963acf2afd3354b3fb9886ef3c
won <dyrhl4321@gmail.com>
2026-08-04 00:57:37 +0900

    fix(home): 브라우저 자동 텍스트 확대 우회 - 16px로 그리고 transform scale로 축소 (sw v311)

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 ﻿// 배포마다 이 버전을 올려야 자동 새버전 적용(새로고침)이 동작함
-const CACHE_NAME = 'murpy-v310';
-const STATIC_CACHE = 'murpy-static-v310';
-const CDN_CACHE = 'murpy-cdn-v310';
+const CACHE_NAME = 'murpy-v311';
+const STATIC_CACHE = 'murpy-static-v311';
+const CDN_CACHE = 'murpy-cdn-v311';
 // 이미지 캐시는 버전 안 붙임 → 코드/HTML 배포해도 유지(URL이 곧 버전)
 const IMG_CACHE = 'murpy-img';
 
