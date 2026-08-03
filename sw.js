9d4de1e113d14aebb8f4f599dc9057300e937360
won <dyrhl4321@gmail.com>
2026-08-03 22:01:04 +0900

    feat(home): 피드 사진에 머피캠 필터 - 캐릭터+오운완 도장 (sw v289)

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 ﻿// 배포마다 이 버전을 올려야 자동 새버전 적용(새로고침)이 동작함
-const CACHE_NAME = 'murpy-v288';
-const STATIC_CACHE = 'murpy-static-v288';
-const CDN_CACHE = 'murpy-cdn-v288';
+const CACHE_NAME = 'murpy-v289';
+const STATIC_CACHE = 'murpy-static-v289';
+const CDN_CACHE = 'murpy-cdn-v289';
 // 이미지 캐시는 버전 안 붙임 → 코드/HTML 배포해도 유지(URL이 곧 버전)
 const IMG_CACHE = 'murpy-img';
 
