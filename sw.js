bcee8b71dd84a15c47d7ace1946cde37c0a8fe50
won <dyrhl4321@gmail.com>
2026-08-04 00:06:29 +0900

    fix(home): 스크롤 잠금 두 방식 충돌 해소 + 머피캠 캡션을 필름바에 픽셀폰트로 직접 타이핑 (sw v300)

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 ﻿// 배포마다 이 버전을 올려야 자동 새버전 적용(새로고침)이 동작함
-const CACHE_NAME = 'murpy-v299';
-const STATIC_CACHE = 'murpy-static-v299';
-const CDN_CACHE = 'murpy-cdn-v299';
+const CACHE_NAME = 'murpy-v300';
+const STATIC_CACHE = 'murpy-static-v300';
+const CDN_CACHE = 'murpy-cdn-v300';
 // 이미지 캐시는 버전 안 붙임 → 코드/HTML 배포해도 유지(URL이 곧 버전)
 const IMG_CACHE = 'murpy-img';
 
