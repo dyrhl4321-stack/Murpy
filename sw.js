abee970970eabaf36447c4ce10b2e29ba5a3a71f
won <dyrhl4321@gmail.com>
2026-08-04 16:25:22 +0900

    feat(shop): 증명형 아이템 퀘스트화 - 진행률 표시(12/30)+안내 팝업+달성 시 지급, price 0 자동보유 버그 수정

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 ﻿// 배포마다 이 버전을 올려야 자동 새버전 적용(새로고침)이 동작함
-const CACHE_NAME = 'murpy-v343';
-const STATIC_CACHE = 'murpy-static-v343';
-const CDN_CACHE = 'murpy-cdn-v343';
+const CACHE_NAME = 'murpy-v344';
+const STATIC_CACHE = 'murpy-static-v344';
+const CDN_CACHE = 'murpy-cdn-v344';
 // 이미지 캐시는 버전 안 붙임 → 코드/HTML 배포해도 유지(URL이 곧 버전)
 const IMG_CACHE = 'murpy-img';
 
