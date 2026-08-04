f2e4bdd8ee6e908e8e7359de2efcdd3559808e77
won <dyrhl4321@gmail.com>
2026-08-04 15:54:36 +0900

    feat(mw): 링 메뉴 4방향 완성 - 캐릭터 정보(메이플식 장비창)/콕 찌르기 추가, 캐릭터-방 좋아요 신설

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 ﻿// 배포마다 이 버전을 올려야 자동 새버전 적용(새로고침)이 동작함
-const CACHE_NAME = 'murpy-v340';
-const STATIC_CACHE = 'murpy-static-v340';
-const CDN_CACHE = 'murpy-cdn-v340';
+const CACHE_NAME = 'murpy-v341';
+const STATIC_CACHE = 'murpy-static-v341';
+const CDN_CACHE = 'murpy-cdn-v341';
 // 이미지 캐시는 버전 안 붙임 → 코드/HTML 배포해도 유지(URL이 곧 버전)
 const IMG_CACHE = 'murpy-img';
 
