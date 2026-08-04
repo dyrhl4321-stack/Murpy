7a0a2804a789dc29102fe8745bdb2aababd10e64
won <dyrhl4321@gmail.com>
2026-08-04 10:54:02 +0900

    feat(splash): 5번째 페이지 머피월드 광고 추가 - 마무리 CTA를 여기로 (sw v316)

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 ﻿// 배포마다 이 버전을 올려야 자동 새버전 적용(새로고침)이 동작함
-const CACHE_NAME = 'murpy-v315';
-const STATIC_CACHE = 'murpy-static-v315';
-const CDN_CACHE = 'murpy-cdn-v315';
+const CACHE_NAME = 'murpy-v316';
+const STATIC_CACHE = 'murpy-static-v316';
+const CDN_CACHE = 'murpy-cdn-v316';
 // 이미지 캐시는 버전 안 붙임 → 코드/HTML 배포해도 유지(URL이 곧 버전)
 const IMG_CACHE = 'murpy-img';
 
