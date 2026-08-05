d734894ff037c1e92800a7f6450fde86ac62c219
won <dyrhl4321@gmail.com>
2026-08-05 13:56:43 +0900

    feat(room): 인테리어 슬롯 확장권 40머피(10칸씩, 15->35 상한)

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 ﻿// 배포마다 이 버전을 올려야 자동 새버전 적용(새로고침)이 동작함
-const CACHE_NAME = 'murpy-v360';
-const STATIC_CACHE = 'murpy-static-v360';
-const CDN_CACHE = 'murpy-cdn-v360';
+const CACHE_NAME = 'murpy-v361';
+const STATIC_CACHE = 'murpy-static-v361';
+const CDN_CACHE = 'murpy-cdn-v361';
 // 이미지 캐시는 버전 안 붙임 → 코드/HTML 배포해도 유지(URL이 곧 버전)
 const IMG_CACHE = 'murpy-img';
 
