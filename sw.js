e01d868d4d496cecbacade9a4a9e31e8d2329f88
won <dyrhl4321@gmail.com>
2026-08-11 10:49:16 +0900

    fix(bump): 일촌 선반을 프로필 한 곳으로 (머피월드 캐릭터 탭에서 제거, sw v439)

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 ﻿// 배포마다 이 버전을 올려야 자동 새버전 적용(새로고침)이 동작함
-const CACHE_NAME = 'murpy-v438';
-const STATIC_CACHE = 'murpy-static-v438';
-const CDN_CACHE = 'murpy-cdn-v438';
+const CACHE_NAME = 'murpy-v439';
+const STATIC_CACHE = 'murpy-static-v439';
+const CDN_CACHE = 'murpy-cdn-v439';
 // 이미지 캐시는 버전 안 붙임 → 코드/HTML 배포해도 유지(URL이 곧 버전)
 const IMG_CACHE = 'murpy-img';
 
